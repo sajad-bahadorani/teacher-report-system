6d91552033f215f993b3db343fc58d3f0c396bd6
Sajad <sajad.bahadorani39@gmail.com>
2026-08-03 18:02:37 +0330

    Add apps folder & Update settings in config

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "apps.accounts",
+    "apps.education",
+    "apps.reports",
+    "apps.finance",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
