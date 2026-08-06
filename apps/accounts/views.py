e4e4d2d5e9b372ef2856fec3a8c9cb5fa1f711d0
Sajad <sajad.bahadorani39@gmail.com>
2026-08-06 22:29:06 +0330

    Update views in accounts app

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import UserSerializer
+from .permissions import IsTeacher, IsEducationOfficer, IsFinanceOfficer
+
 
 
 class ProfileView(APIView):
@@ -11,4 +13,27 @@ class ProfileView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class TeacherDashboardView(APIView):
+    permission_classes = [IsAuthenticated, IsTeacher]
+
+    def get(self, request):
+        return Response({"message": "Welcome Teacher"})
+
+
+class EducationDashboardView(APIView):
+    permission_classes = [IsAuthenticated, IsEducationOfficer]
+
+    def get(self, request):
+        return Response({"message": "Welcome Education Officer"})
+
+
+class FinanceDashboardView(APIView):
+    permission_classes = [IsAuthenticated, IsFinanceOfficer]
+
+    def get(self, request):
+        return Response({"message": "Welcome Finance Officer"})
+    
+    
 
