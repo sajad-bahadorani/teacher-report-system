6f8970abafb49bfedf7ea2ab661fa9ca9b92e210
Sajad <sajad.bahadorani39@gmail.com>
2026-08-06 22:28:24 +0330

    Update urls in accounts app

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import ProfileView
+from .views import ProfileView, TeacherDashboardView, EducationDashboardView, FinanceDashboardView
 
 
 urlpatterns = [
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("me/", ProfileView.as_view(), name="me"),
+    path("teacher/", TeacherDashboardView.as_view(), name="teacher"),
+    path("education/", EducationDashboardView.as_view(), name="education"),
+    path("finance/", FinanceDashboardView.as_view(), name="finance"),
 ]
