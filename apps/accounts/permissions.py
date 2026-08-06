ba4619582f51135417616ade29765c195c9cfd2a
Sajad <sajad.bahadorani39@gmail.com>
2026-08-06 22:26:07 +0330

    Update permissions in accounts app

--- apps/accounts/permissions.py
+++ apps/accounts/permissions.py
@@ -1,19 +1,21 @@
 from rest_framework.permissions import BasePermission
 
+from .models import User
+
 
 class IsTeacher(BasePermission):
     
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == "teacher"
+        return request.user.is_authenticated and request.user.role == User.Role.TEACHER
     
     
 class IsEducationOfficer(BasePermission):
     
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == "education"
+        return request.user.is_authenticated and request.user.role == User.Role.EDUCATION_OFFICER
     
     
 class IsFinanceOfficer(BasePermission):
     
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == "finance"
+        return request.user.is_authenticated and request.user.role == User.Role.FINANCE_OFFICER
