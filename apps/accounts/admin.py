bbd5f02f7dfdf6ec857f3277d8110c076858e979
Sajad <sajad.bahadorani39@gmail.com>
2026-08-06 08:19:54 +0330

    Update admin file in accounts app

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "full_name",
+        "role",
+        "phone_number",
+        "is_active",
+    )
+
+    list_filter = (
+        "role",
+        "is_active",
+        "is_staff",
+    )
+
+    search_fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "phone_number",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional Information",
+            {
+                "fields": (
+                    "phone_number",
+                    "emergency_phone",
+                    "role",
+                )
+            },
+        ),
+    )
